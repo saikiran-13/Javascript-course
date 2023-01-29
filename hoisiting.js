fae454db5654a65e8961a3d1cefb6816a5a51e64
saikiran-13 <saikiran@simformsolutions.com>
2023-01-29 17:35:02 +0530

    all changes done

---
+++ hoisiting.js
@@ -0,0 +1,12 @@
+console.log(x)
+display()
+// console.log(simform)
+console.log(display)
+var x = 156
+function display(){//In memory in memory allocation phase it contains total funciton definition
+    console.log("Display the name of the function-->display")
+}
+const simform = ()=>{//this function is assigned to variable name it gets value undefinged if you call it you will get an error
+    console.log("display your name")
+}
+//run the code in browser console
